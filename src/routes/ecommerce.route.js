702b649be1dd2cf2ae7430f1c09e54143ddddd14
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-02-26 15:14:25 +0530

    feat(routes): add route to get DISTINCT features for a given category

--- src/routes/ecommerce.route.js
+++ src/routes/ecommerce.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const categoryHandler = require('../handlers/ecommerce.handler');
-
+const featureHandler = require("../handlers/features.handler")
 const categoryRouter = express.Router();
 
 categoryRouter.post('/', categoryHandler.createCategoryHandler);
+categoryRouter.get('/', featureHandler.getFeaturesHandler);
 
 module.exports = {
   categoryRouter,
